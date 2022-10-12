8deec592a6d63420e01e2636841d86afdf172ee2
LSYoshioka <111407264+LSYoshioka@users.noreply.github.com>
2022-10-11 21:36:03 -0500

    first commit

---
+++ src/main/java/edu/udea/Nukak/NukakApplication.java
@@ -0,0 +1,13 @@
+package edu.udea.Nukak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NukakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/udea/Nukak/NukakApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udea.Nukak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NukakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
